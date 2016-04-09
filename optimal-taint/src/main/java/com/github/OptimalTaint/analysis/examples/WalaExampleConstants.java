24f0984ee9a8a21bf4bd85825820f159a465b1a9
Jose Cambronero <jose.cambronero@cs.nyu.edu>
2016-04-09 16:12:01 -0400

    changed dot exec

--- optimal-taint/src/main/java/com/github/OptimalTaint/analysis/examples/WalaExampleConstants.java
+++ optimal-taint/src/main/java/com/github/OptimalTaint/analysis/examples/WalaExampleConstants.java
@@ -4,7 +4,7 @@ package com.github.OptimalTaint.analysis.examples;
  * Holds some simple constants associated with utilities used by examples
  */
 public final class WalaExampleConstants {
-    static final String DOT_EXE = "/usr/local/bin/dot";
+    static final String DOT_EXE = "dot";
     static final String GV_EXE = "gv";
     private WalaExampleConstants(){}
 }
